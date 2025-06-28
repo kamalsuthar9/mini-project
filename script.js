49e10f6430c2173e1d6198abc31a68a141aefb47
kamalsuthar9 <kamalsuthar1008@gmail.com>
2025-06-28 18:28:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Redirect to services page when a specific condition is met
+function redirectToServices() {
+    alert("Redirecting to services...");
+    window.location.href = "services.html";
+  }
+  
